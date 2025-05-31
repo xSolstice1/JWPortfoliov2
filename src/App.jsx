c5a5bc1c743bd6c7f5b771d9b4942c9d08c100d2
Jinwei Ang <jinwei.ang@dxc.com>
2025-05-31 22:44:28 +0800

    About Section, added intro and tech cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import About from "./sections/About";
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
       <Hero />
-      <section className="min-h-screen" />
+      <About />
       <section className="min-h-screen" />
       <section className="min-h-screen" />
       <section className="min-h-screen" />
---
+++ src/sections/About.jsx
@@ -0,0 +1,118 @@
+import { useRef } from "react";
+import Card from "../components/Card";
+
+const About = () => {
+  const grid2Container = useRef();
+  return (
+    <section className="c-space section-spacing">
+      <h2 className="text-heading">About Me</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
+        {/* Grid 1 */}
+        <div className="flex items-end grid-default-color grid-1">
+          <img
+            src="assets/coding-pov.png"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <div className="z-10">
+            <p className="headtext">Hi, I'm Jinwei</p>
+            <p className="subtext">
+              I've been working as a Data Engineer for slightly over a year
+              right now, I mainly work with SQL in Snowflake and Tableau for
+              data visualization.
+            </p>
+          </div>
+          <div className="absolute inset-x-0 pointer-events-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
+        </div>
+        {/* Grid 2 */}
+        <div className="grid-default-color grid-2">
+          <div
+            ref={grid2Container}
+            className="flex items-center justify-center w-full h-full"
+          >
+            <p className="flex items-end text-5xl text-gray-500"></p>
+            <Card
+              style={{
+                rotate: "75deg",
+                top: "30%",
+                left: "20%",
+                width: "6rem",
+                height: "6rem",
+                objectFit: "contain",
+              }}
+              image="assets/logos/sql.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{
+                rotate: "-30deg",
+                top: "60%",
+                left: "45%",
+                width: "6rem",
+                height: "6rem",
+                objectFit: "contain",
+              }}
+              image="assets/logos/snowflake.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{
+                rotate: "75deg",
+                bottom: "30%",
+                left: "70%",
+                width: "6rem",
+                height: "6rem",
+                objectFit: "contain",
+              }}
+              image="assets/logos/react.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{
+                rotate: "-75deg",
+                top: "70%",
+                left: "20%",
+                width: "6rem",
+                height: "6rem",
+                objectFit: "contain",
+              }}
+              image="assets/logos/visualstudiocode.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{
+                rotate: "30deg",
+                top: "70%",
+                left: "70%",
+                width: "6rem",
+                height: "6rem",
+                objectFit: "contain",
+              }}
+              image="assets/logos/javascript.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{
+                rotate: "30deg",
+                top: "70%",
+                left: "70%",
+                width: "6rem",
+                height: "6rem",
+                objectFit: "contain",
+              }}
+              image="assets/logos/python.png"
+              containerRef={grid2Container}
+            />
+          </div>
+        </div>
+        {/* Grid 3 */}
+        <div className="grid-black-color grid-3"></div>
+        {/* Grid 4 */}
+        <div className="grid-special-color grid-4"></div>
+        {/* Grid 5 */}
+        <div className="grid-default-color grid-5"></div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
