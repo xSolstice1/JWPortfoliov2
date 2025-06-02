21db8efeacac9e78803f57cc6a26f8668c7a52e4
Jinwei Ang <jinwei.ang@dxc.com>
2025-06-02 15:50:12 +0800

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,21 @@ const Footer = () => {
     <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
       <div className="flex gap-2">
-        
+        <p>
+          Built with{" "}
+          <span className="text-white transition-all duration-300 hover:text-blue-500 hover:scale-105 inline-block">
+            React/Vite
+          </span>{" "}
+          and{" "}
+          <span className="text-white transition-all duration-300 hover:text-blue-500 hover:scale-105 inline-block">
+            TailwindCSS
+          </span>
+        </p>
       </div>
       <div className="flex gap-3">
         {mySocials.map((social, index) => (
           <a href={social.href} key={index} target="_blank">
-            <img src={social.icon} className="w-5 h-5" alt={social.name}/>
+            <img src={social.icon} className="w-5 h-5" alt={social.name} />
           </a>
         ))}
       </div>
